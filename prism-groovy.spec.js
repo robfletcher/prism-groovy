7f2e3996fd8641e8da6872488b8d149b2febac7c
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-03 23:06:51 +0100

    collection literals

--- prism-groovy.spec.js
+++ prism-groovy.spec.js
@@ -1,81 +1,101 @@
 describe("Groovy syntax highlighting for Prism.js", function() {
-	beforeEach(function() {
-		this.operators = $('#operators');
-		this.keywords = $('#keywords');
-		this.numbers = $('#numbers');
-	});
 
 	it("highlights operators", function() {
-		expect(this.operators.find('#plus .operator').text()).toBe('+');
-		expect(this.operators.find('#minus .operator').text()).toBe('-');
-		expect(this.operators.find('#multiply .operator').text()).toBe('*');
-		expect(this.operators.find('#divide .operator').text()).toBe('/');
-		expect(this.operators.find('#power .operator').text()).toBe('**');
-		expect(this.operators.find('#mod .operator').text()).toBe('%');
-		expect(this.operators.find('#or .operator').text()).toBe('|');
-		expect(this.operators.find('#and .operator').text()).toBe('&');
-		expect(this.operators.find('#xor .operator').text()).toBe('^');
-		expect(this.operators.find('#next-prefix .operator').text()).toBe('++');
-		expect(this.operators.find('#next-postfix .operator').text()).toBe('++');
-		expect(this.operators.find('#previous-prefix .operator').text()).toBe('--');
-		expect(this.operators.find('#previous-postfix .operator').text()).toBe('--');
-		expect(this.operators.find('#left-shift .operator').text()).toBe('<<');
-		expect(this.operators.find('#right-shift .operator').text()).toBe('>>');
-		expect(this.operators.find('#bitwise-negate .operator').text()).toBe('~');
-		expect(this.operators.find('#negative .operator').text()).toBe('-');
-		expect(this.operators.find('#positive .operator').text()).toBe('+');
-		expect(this.operators.find('#assignment .operator').text()).toBe('=');
-		expect(this.operators.find('#equals .operator').text()).toBe('==');
-		expect(this.operators.find('#not-equals .operator').text()).toBe('!=');
-		expect(this.operators.find('#spaceship .operator').text()).toBe('<=>');
-		expect(this.operators.find('#gt .operator').text()).toBe('>');
-		expect(this.operators.find('#gte .operator').text()).toBe('>=');
-		expect(this.operators.find('#lt .operator').text()).toBe('<');
-		expect(this.operators.find('#lte .operator').text()).toBe('<=');
-		expect(this.operators.find('#dereference .operator').text()).toBe('.');
-		expect(this.operators.find('#safe-dereference .operator').text()).toBe('?.');
-		expect(this.operators.find('#spread .operator').text()).toBe('*.');
-		expect(this.operators.find('#java-field .operator').text()).toBe('.@');
-		expect(this.operators.find('#spread-java-field .operator').text()).toBe('*.@');
-		expect(this.operators.find('#method-ref .operator').text()).toBe('.&');
-		expect(this.operators.find('#elvis .operator').text()).toBe('?:');
-		expect(this.operators.find('#regex .operator').text()).toBe('=~');
-		expect(this.operators.find('#firecracker .operator').text()).toBe('==~');
+		expect($('#plus .operator').text()).toBe('+');
+		expect($('#minus .operator').text()).toBe('-');
+		expect($('#multiply .operator').text()).toBe('*');
+		expect($('#divide .operator').text()).toBe('/');
+		expect($('#power .operator').text()).toBe('**');
+		expect($('#mod .operator').text()).toBe('%');
+		expect($('#or .operator').text()).toBe('|');
+		expect($('#and .operator').text()).toBe('&');
+		expect($('#xor .operator').text()).toBe('^');
+		expect($('#next-prefix .operator').text()).toBe('++');
+		expect($('#next-postfix .operator').text()).toBe('++');
+		expect($('#previous-prefix .operator').text()).toBe('--');
+		expect($('#previous-postfix .operator').text()).toBe('--');
+		expect($('#left-shift .operator').text()).toBe('<<');
+		expect($('#right-shift .operator').text()).toBe('>>');
+		expect($('#bitwise-negate .operator').text()).toBe('~');
+		expect($('#negative .operator').text()).toBe('-');
+		expect($('#positive .operator').text()).toBe('+');
+		expect($('#assignment .operator').text()).toBe('=');
+		expect($('#equals .operator').text()).toBe('==');
+		expect($('#not-equals .operator').text()).toBe('!=');
+		expect($('#spaceship .operator').text()).toBe('<=>');
+		expect($('#gt .operator').text()).toBe('>');
+		expect($('#gte .operator').text()).toBe('>=');
+		expect($('#lt .operator').text()).toBe('<');
+		expect($('#lte .operator').text()).toBe('<=');
+		expect($('#dereference .operator').text()).toBe('.');
+		expect($('#safe-dereference .operator').text()).toBe('?.');
+		expect($('#spread .operator').text()).toBe('*.');
+		expect($('#java-field .operator').text()).toBe('.@');
+		expect($('#spread-java-field .operator').text()).toBe('*.@');
+		expect($('#method-ref .operator').text()).toBe('.&');
+		expect($('#elvis .operator').text()).toBe('?:');
+		expect($('#regex .operator').text()).toBe('=~');
+		expect($('#firecracker .operator').text()).toBe('==~');
 	});
 
 	it("highlights index operator", function() {
-		var punctuations = this.operators.find('#get-at .punctuation');
-		expect(punctuations.eq(0).text()).toBe('[');
-		expect(punctuations.eq(1).text()).toBe(']');
+		expect($('#get-at .punctuation').eq(0).text()).toBe('[');
+		expect($('#get-at .punctuation').eq(1).text()).toBe(']');
+	});
+
+	it("highlights ternary operator", function() {
+		expect($('#ternary .operator').eq(0).text()).toBe('?');
+		expect($('#ternary .operator').eq(1).text()).toBe(':');
 	});
 
 	it("highlights 'keyword' operators", function() {
-		expect(this.operators.find('#instanceof .keyword').text()).toBe('instanceof');
-		expect(this.operators.find('#cast .keyword').text()).toBe('as');
+		expect($('#instanceof .keyword').text()).toBe('instanceof');
+		expect($('#cast .keyword').text()).toBe('as');
 	});
 
 	it("highlights Groovy keywords", function() {
-		expect(this.keywords.find('#def-var .keyword').text()).toBe('def');
-		expect(this.keywords.find('#def-return .keyword').text()).toBe('def');
-		expect(this.keywords.find('#def-param .keyword').eq(1).text()).toBe('def');
-		expect(this.keywords.find('#import-as .keyword').eq(1).text()).toBe('as');
-		expect(this.keywords.find('#in .keyword').eq(1).text()).toBe('in');
+		expect($('#def-var .keyword').text()).toBe('def');
+		expect($('#def-return .keyword').text()).toBe('def');
+		expect($('#def-param .keyword').eq(1).text()).toBe('def');
+		expect($('#import-as .keyword').eq(1).text()).toBe('as');
+		expect($('#in .keyword').eq(1).text()).toBe('in');
 	});
 
 	it("highlights numeric literals", function() {
-		expect(this.numbers.find('#int .number').text()).toBe('1234567890');
-		expect(this.numbers.find('#decimal .number').text()).toBe('12345.67890');
-		expect(this.numbers.find('#binary .number').text()).toBe('0b01');
-		expect(this.numbers.find('#hex .number').text()).toBe('0x123456789abcdef0');
-		expect(this.numbers.find('#float .number').text()).toBe('1.1f');
-		expect(this.numbers.find('#integer .number').text()).toBe('1i');
-		expect(this.numbers.find('#long .number').text()).toBe('1L');
-		expect(this.numbers.find('#big-integer .number').text()).toBe('1g');
-		expect(this.numbers.find('#big-decimal .number').text()).toBe('1.1g');
+		expect($('#int .number').text()).toBe('1234567890');
+		expect($('#negative-int .number').text()).toBe('-1');
+		expect($('#decimal .number').text()).toBe('12345.67890');
+		expect($('#binary .number').text()).toBe('0b01');
+		expect($('#hex .number').text()).toBe('0x123456789abcdef0');
+		expect($('#float .number').text()).toBe('1.1f');
+		expect($('#integer .number').text()).toBe('1i');
+		expect($('#long .number').text()).toBe('1L');
+		expect($('#big-integer .number').text()).toBe('1g');
+		expect($('#big-decimal .number').text()).toBe('1.1g');
 	});
 
 	it("highlights Java 7 style numeric literals", function() {
-		expect(this.numbers.find('#underscore-int .number').text()).toBe('1_000');
-		expect(this.numbers.find('#underscore-decimal .number').text()).toBe('1_000.000_1');
+		expect($('#underscore-int .number').text()).toBe('1_000');
+		expect($('#underscore-decimal .number').text()).toBe('1_000.000_1');
+	});
+
+	it("highlights list literals", function() {
+		expect($('#list .punctuation').eq(0).text()).toBe('[');
+		expect($('#list .punctuation').eq(1).text()).toBe(',');
+		expect($('#list .punctuation').eq(2).text()).toBe(',');
+		expect($('#list .punctuation').eq(3).text()).toBe(']');
+	});
+
+	it("highlights map literals", function() {
+		expect($('#map .punctuation').eq(0).text()).toBe('[');
+		expect($('#map .punctuation').eq(1).text()).toBe(':');
+		expect($('#map .punctuation').eq(2).text()).toBe(',');
+		expect($('#map .punctuation').eq(3).text()).toBe(':');
+		expect($('#map .punctuation').eq(4).text()).toBe(']');
+	});
+
+	it("highlights range literals", function() {
+		expect($('#range .operator').text()).toBe('..');
+		expect($('#half-ex-range .operator').text()).toBe('..<');
 	});
 });

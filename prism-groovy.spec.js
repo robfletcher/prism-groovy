c414c4fc55baa329f1c0b6527bd3b896243fcf20
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-03 22:16:07 +0100

    completed tests for operators

--- prism-groovy.spec.js
+++ prism-groovy.spec.js
@@ -13,5 +13,41 @@ describe("Groovy syntax highlighting for Prism.js", function() {
 		expect(this.operators.find('#or .operator').text()).toBe('|');
 		expect(this.operators.find('#and .operator').text()).toBe('&');
 		expect(this.operators.find('#xor .operator').text()).toBe('^');
+		expect(this.operators.find('#next-prefix .operator').text()).toBe('++');
+		expect(this.operators.find('#next-postfix .operator').text()).toBe('++');
+		expect(this.operators.find('#previous-prefix .operator').text()).toBe('--');
+		expect(this.operators.find('#previous-postfix .operator').text()).toBe('--');
+		expect(this.operators.find('#left-shift .operator').text()).toBe('<<');
+		expect(this.operators.find('#right-shift .operator').text()).toBe('>>');
+		expect(this.operators.find('#bitwise-negate .operator').text()).toBe('~');
+		expect(this.operators.find('#negative .operator').text()).toBe('-');
+		expect(this.operators.find('#positive .operator').text()).toBe('+');
+		expect(this.operators.find('#equals .operator').text()).toBe('==');
+		expect(this.operators.find('#not-equals .operator').text()).toBe('!=');
+		expect(this.operators.find('#spaceship .operator').text()).toBe('<=>');
+		expect(this.operators.find('#gt .operator').text()).toBe('>');
+		expect(this.operators.find('#gte .operator').text()).toBe('>=');
+		expect(this.operators.find('#lt .operator').text()).toBe('<');
+		expect(this.operators.find('#lte .operator').text()).toBe('<=');
+		expect(this.operators.find('#dereference .operator').text()).toBe('.');
+		expect(this.operators.find('#safe-dereference .operator').text()).toBe('?.');
+		expect(this.operators.find('#spread .operator').text()).toBe('*.');
+		expect(this.operators.find('#java-field .operator').text()).toBe('.@');
+		expect(this.operators.find('#spread-java-field .operator').text()).toBe('*.@');
+		expect(this.operators.find('#method-ref .operator').text()).toBe('.&');
+		expect(this.operators.find('#elvis .operator').text()).toBe('?:');
+		expect(this.operators.find('#regex .operator').text()).toBe('=~');
+		expect(this.operators.find('#firecracker .operator').text()).toBe('==~');
+	});
+
+	it("highlights index operator", function() {
+		var punctuations = this.operators.find('#get-at .punctuation');
+		expect(punctuations.eq(0).text()).toBe('[');
+		expect(punctuations.eq(1).text()).toBe(']');
+	});
+
+	it("highlights 'keyword' operators", function() {
+		expect(this.operators.find('#instanceof .keyword').text()).toBe('instanceof');
+		expect(this.operators.find('#cast .keyword').text()).toBe('as');
 	});
 });

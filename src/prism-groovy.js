64dd7cad2cc4eb6c8bbe41470422d24a7a80b75b
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-08 08:54:11 +0100

    stopped highlighting expressions in single-quoted strings

--- src/prism-groovy.js
+++ src/prism-groovy.js
@@ -2,6 +2,7 @@ Prism.languages.groovy = Prism.languages.extend('clike', {
 	'keyword': /\b(as|def|in|abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b/g,
 	'number': /\b0b[01]+\b|\b0x[\da-f]+(\.[\da-fp\-]+)?\b|\b\d+(\.\d+[e]?[\d]*)?[glidf]\b|\d+(\.\d+)?\b/gi,
 	'operator': /={0,2}~|\?\.|\*?\.@|\.&amp;|\.(?=\w)|\.{2}(&lt;)?(?=\w)|-&gt;|\?:|[-+]{1,2}|!|(&lt;=)?&gt;|&lt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\/|\^|%/g,
+    'string': /("""|''')[\W\w]*?\1|("|'|\/)[\W\w]*?\2/g,
     'annotation': /@\w+/
 });
 
@@ -9,17 +10,16 @@ Prism.languages.insertBefore('groovy', 'punctuation', {
 	'block-label': /\b(setup|given|when|then|and|cleanup|expect|where):/g
 });
 
-Prism.languages.insertBefore('groovy', 'keyword', {
-	'string': {
-		pattern: /("""|''')[\W\w]*?\1|("|'|\/).*?\2/g,
-		inside: {
-			'expression': {
-				pattern: /([^\\])(\$(\{.*?\}|[\w\.]*))/,
-				lookbehind: true,
-				inside: Prism.languages.extend('groovy', {
-					'punctuation': /[{}[\];(),.:$]/g
-				})
-			}
-		}
+Prism.hooks.add('wrap', function(env) {
+	if (env.type === 'string' && env.content[0] != "'") {
+		env.content = Prism.highlight(env.content, {
+    		'expression': {
+    			pattern: /([^\\])(\$(\{.*?\}|[\w\.]*))/,
+		    	lookbehind: true,
+    			inside: Prism.languages.extend('groovy', {
+    				'punctuation': /[{}[\];(),.:$]/g
+    			})
+    		}
+		});
 	}
 });

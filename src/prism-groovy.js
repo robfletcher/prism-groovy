7fe322718cc9ec23710b8cfc0695e6927fe3f6ee
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-08 09:17:18 +0100

    underscores in numeric literals

--- src/prism-groovy.js
+++ src/prism-groovy.js
@@ -1,6 +1,6 @@
 Prism.languages.groovy = Prism.languages.extend('clike', {
 	'keyword': /\b(as|def|in|abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b/g,
-	'number': /\b0b[01]+\b|\b0x[\da-f]+(\.[\da-fp\-]+)?\b|\b\d+(\.\d+[e]?[\d]*)?[glidf]\b|\d+(\.\d+)?\b/gi,
+    'number': /\b0b[01_]+\b|\b0x[\da-f_]+(\.[\da-f_p\-]+)?\b|\b[\d_]+(\.[\d_]+[e]?[\d]*)?[glidf]\b|[\d_]+(\.[\d_]+)?\b/gi,
 	'operator': /={0,2}~|\?\.|\*?\.@|\.&amp;|\.(?=\w)|\.{2}(&lt;)?(?=\w)|-&gt;|\?:|[-+]{1,2}|!|(&lt;=)?&gt;|&lt;|={1,2}|(&amp;){1,2}|\|?\||\?|\*|\/|\^|%/g,
     'string': /("""|''')[\W\w]*?\1|("|'|\/)[\W\w]*?\2/g,
     'annotation': /@\w+/
